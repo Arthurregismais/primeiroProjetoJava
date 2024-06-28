2938dd07d4ec75609663ba1e7fe89bcffc625c77
Arthurregismais <Arthur@DESKTOP-PRVT7GH>
2024-06-27 22:58:39 -0300

    Criando um objeto com as disciplinas do aluno

--- PrimeiraClasseJava/src/cursojava/classes/Aluno.java
+++ PrimeiraClasseJava/src/cursojava/classes/Aluno.java
@@ -15,45 +15,12 @@ public class Aluno {
 	private String nomeMae;
 	private String nomePai;
 	private String dataMatricula;
-	
-	public double getNota1() {
-		return nota1;
-	}
-
-	public void setNota1(double nota1) {
-		this.nota1 = nota1;
-	}
-
-	public double getNota2() {
-		return nota2;
-	}
-
-	public void setNota2(double nota2) {
-		this.nota2 = nota2;
-	}
-
-	public double getNota3() {
-		return nota3;
-	}
-
-	public void setNota3(double nota3) {
-		this.nota3 = nota3;
-	}
-
-	public double getNota4() {
-		return nota4;
-	}
-
-	public void setNota4(double nota4) {
-		this.nota4 = nota4;
-	}
-
 	private String nomeEscola;
 	private String serieMatriculado;
-	private double nota1;
-	private double nota2;
-	private double nota3;
-	private double nota4;
+	
+	private Disciplina disciplina = new Disciplina();
+
+	
 	
 	
 	public int getIdade() {
@@ -157,7 +124,7 @@ public class Aluno {
 	}
 	
 	public double getMediaNota() {
-		return (nota1 + nota2 + nota3 + nota4) / 4;
+		return (disciplina.getNota1() + disciplina.getNota2() + disciplina.getNota3() + disciplina.getNota4()) / 4 ;
 	}
 	
 	
@@ -181,15 +148,6 @@ public class Aluno {
 		else {
 			return false;
 		}
-	}
-
-	@Override
-	public String toString() {
-		return "Aluno [nome=" + nome + ", idade=" + idade + ", dataNascimento=" + dataNascimento + ", registroGeral="
-				+ registroGeral + ", numeroCpf=" + numeroCpf + ", nomeMae=" + nomeMae + ", nomePai=" + nomePai
-				+ ", dataMatricula=" + dataMatricula + ", nomeEscola=" + nomeEscola + ", serieMatriculado="
-				+ serieMatriculado + ", nota1=" + nota1 + ", nota2=" + nota2 + ", nota3=" + nota3 + ", nota4=" + nota4
-				+ "]";
 	}
 
 	@Override
@@ -221,6 +179,14 @@ public class Aluno {
 		} else if (!numeroCpf.equals(other.numeroCpf))
 			return false;
 		return true;
+	}
+
+	public Disciplina getDisciplina() {
+		return disciplina;
+	}
+
+	public void setDisciplina(Disciplina disciplina) {
+		this.disciplina = disciplina;
 	}
 	
 	
